bd8682df97a7183b4b839f66ded31e840639882a
enrique & johnyoung <student@epicodus.com>
2015-04-27 17:03:14 -0700

    final save

--- app.rb
+++ app.rb
@@ -11,9 +11,8 @@ get('/') do
 end
 
 get('/results') do
-  @triangle = params.fetch("triangle")
-
-
+  newT = Triangle.new(params.fetch("triangle")
+  @result = newT.shapes?
   #@user_string = params.fetch('user_string')
   #@self = params.fetch('self')
   #@times = @user_string.split(/ /).count(@self)
